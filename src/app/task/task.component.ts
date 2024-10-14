b4415025a43692660fc7092171cc320554f42400
shaodado <shaoshineshine@gamil.com>
2024-10-14 13:46:52 +0800

    利用 setter 屬性傳入資料轉換成數值型別

--- src/app/task/task.component.ts
+++ src/app/task/task.component.ts
@@ -8,9 +8,18 @@ import { Component, Input } from '@angular/core';
   styleUrl: './task.component.css',
 })
 export class TaskComponent {
+  private _id!: number;
   @Input() content!: string;
   @Input() type!: 'Home' | 'Work' | 'Study' | 'Other';
   @Input() state!: 'None' | 'Doing' | 'Finish' | 'Finish';
+
+  @Input() set id(id: string) {
+    this._id = +id;
+  }
+
+  get id(): string {
+    return this._id.toString();
+  }
 
   onSetState(state: 'None' | 'Doing' | 'Finish' | 'Finish'): void {
     this.state = state;
