10309ee3bc4fbc3fd377b96b3e8c7ec9edb5ad18
shaodado <shaoshineshine@gamil.com>
2024-10-16 12:58:50 +0800

    渲染頁面結果

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,6 +13,8 @@ export class AppComponent {
   content = '建立代辦事項元件';
   type: 'Home' | 'Work' | 'Study' | 'Other' = 'Home';
   state: 'None' | 'Doing' | 'Finish' | 'Finish' = 'None';
+  totalCount = 10;
+  finishCount = 3;
   onSetState(state: 'None' | 'Doing' | 'Finish' | 'Finish'): void {
     this.state = state;
   }
